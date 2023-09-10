13dc4557a7daf6af6c79820c54baaa5512af709c
nguyễn phong <phongp1192002@phongp1192002.onmicrosoft.com>
2023-09-10 16:42:38 +0700

    Quản lý bài viết & Phân trang TIn tức

--- WebBanHangOnline/Areas/Admin/Controllers/PostsController.cs
+++ WebBanHangOnline/Areas/Admin/Controllers/PostsController.cs
@@ -1,5 +1,4 @@
-﻿using PagedList;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,26 +8,13 @@ using WebBanHangOnline.Models.EF;
 
 namespace WebBanHangOnline.Areas.Admin.Controllers
 {
-    public class NewsController : Controller
+    public class PostsController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
-        // GET: Admin/News
-        public ActionResult Index(string SearchText, int? page)
+        // GET: Admin/Posts
+        public ActionResult Index()
         {
-            var pageSize = 2;
-            if (page == null)
-            {
-                page = 1;
-            }
-            IEnumerable<News> items = db.News.OrderByDescending(x => x.Id);
-            if (!string.IsNullOrEmpty(SearchText))
-            {
-                items = items.Where(x => x.Alias.Contains(SearchText) || x.Title.Contains(SearchText)).ToList();
-            }
-            var pageIndex = page.HasValue ? Convert.ToInt32(page) : 1;
-            items =items.ToPagedList(pageIndex, pageSize);
-            ViewBag.PageSize = pageSize;
-            ViewBag.Page = page;
+            var items = db.Posts.OrderByDescending(x => x.Id).ToList();
             return View(items);
         }
         public ActionResult Add()
@@ -37,35 +23,35 @@ namespace WebBanHangOnline.Areas.Admin.Controllers
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Add(News model)
+        public ActionResult Add(Posts model)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
-                model.CreatedDate = DateTime.Now;   
+                model.CreatedDate = DateTime.Now;
                 model.ModifiedDate = DateTime.Now;
                 model.CategoryId = 3;
                 model.Alias = WebBanHangOnline.Models.Common.Filter.FilterChar(model.Title);
-                db.News.Add(model);
+                db.Posts.Add(model);
                 db.SaveChanges();
-                return RedirectToAction("Index");   
+                return RedirectToAction("Index");
             }
             return View(model);
         }
         public ActionResult Edit(int id)
         {
-            var item = db.News.Find(id);
+            var item = db.Posts.Find(id);
             return View(item);
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(News model)
+        public ActionResult Edit(Posts model)
         {
             if (ModelState.IsValid)
             {
                 model.ModifiedDate = DateTime.Now;
                 model.CategoryId = 3;
                 model.Alias = WebBanHangOnline.Models.Common.Filter.FilterChar(model.Title);
-                db.News.Attach(model);
+                db.Posts.Attach(model);
                 db.Entry(model).State = System.Data.Entity.EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -75,48 +61,46 @@ namespace WebBanHangOnline.Areas.Admin.Controllers
         [HttpPost]
         public ActionResult Delete(int id)
         {
-            var item = db.News.Find(id);
-            if(item != null)
+            var item = db.Posts.Find(id);
+            if (item != null)
             {
-                db.News.Remove(item);
+                db.Posts.Remove(item);
                 db.SaveChanges();
                 return Json(new { success = true });
             }
-            return Json(new {success = false});
+            return Json(new { success = false });
         }
         [HttpPost]
         public ActionResult IsActice(int id)
         {
-            var item = db.News.Find(id);
+            var item = db.Posts.Find(id);
             if (item != null)
             {
                 item.IsActice = !item.IsActice;
                 db.Entry(item).State = System.Data.Entity.EntityState.Modified;
                 db.SaveChanges();
-                return Json(new { success = true, isActice = item.IsActice});
+                return Json(new { success = true, isActice = item.IsActice });
             }
             return Json(new { success = false });
         }
         [HttpPost]
         public ActionResult DeleteAll(string ids)
         {
-            if(!string.IsNullOrEmpty(ids))
+            if (!string.IsNullOrEmpty(ids))
             {
                 var items = ids.Split(',');
-                if(items !=null && items.Any())
+                if (items != null && items.Any())
                 {
-                    foreach(var item in items) 
+                    foreach (var item in items)
                     {
-                        var obj = db.News.Find(Convert.ToInt32(item));
-                        db.News.Remove(obj);
+                        var obj = db.Posts.Find(Convert.ToInt32(item));
+                        db.Posts.Remove(obj);
                         db.SaveChanges();
                     }
                     return Json(new { success = true });
                 }
             }
             return Json(new { success = false });
-        }   
-
-
+        }
     }
 }
