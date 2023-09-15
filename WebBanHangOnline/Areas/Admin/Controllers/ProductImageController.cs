d68c02b7b37661dfc9ee51b76069869ea37ff314
nguyễn phong <phongp1192002@phongp1192002.onmicrosoft.com>
2023-09-15 21:59:21 +0700

    Thêm xóa sửa sản phẩm, upload nhiều ảnh và dùng autonumber cho giá

--- WebBanHangOnline/Areas/Admin/Controllers/ProductImageController.cs
+++ WebBanHangOnline/Areas/Admin/Controllers/ProductImageController.cs
@@ -12,10 +12,53 @@ namespace WebBanHangOnline.Areas.Admin.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
         // GET: Admin/ProductImages
-        public ActionResult Index(int productId)
+        public ActionResult Index(int id)
         {
-            var items = db.ProductImages.Where(x=>x.ProductId == productId).ToList();
-            return View();
+            ViewBag.ProductId = id;
+            var items = db.ProductImages.Where(x=>x.ProductId == id).ToList();
+            return View(items);
+        }
+        public ActionResult AddImage(int productid, string url)
+        {
+            db.ProductImages.Add(new ProductImage
+            {
+                ProductId = productid,
+                Image = url,
+                IsDefault = false
+            });
+            db.SaveChanges();
+            return Json(new { success = true });
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var item = db.ProductImages.Find(id);
+            if (item != null)
+            {
+                db.ProductImages.Remove(item);
+                db.SaveChanges();
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+        [HttpPost]
+        public ActionResult DeleteAll(string ids)
+        {
+            if (!string.IsNullOrEmpty(ids))
+            {
+                var items = ids.Split(',');
+                if (items != null && items.Any())
+                {
+                    foreach (var item in items)
+                    {
+                        var obj = db.ProductImages.Find(Convert.ToInt32(item));
+                        db.ProductImages.Remove(obj);
+                        db.SaveChanges();
+                    }
+                    return Json(new { success = true });
+                }
+            }
+            return Json(new { success = false });
         }
     }
 }
