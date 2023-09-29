a2a21ce933688ea294395b400a559076d6fc3e0f
nguyễn phong <phongp1192002@phongp1192002.onmicrosoft.com>
2023-09-29 21:43:53 +0700

    tạo trang danh mục sản phẩm

--- WebBanHangOnline/Controllers/ProductController.cs
+++ WebBanHangOnline/Controllers/ProductController.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using PagedList;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 using WebBanHangOnline.Models;
+using WebBanHangOnline.Models.EF;
 
 namespace WebBanHangOnline.Controllers
 {
@@ -11,9 +14,47 @@ namespace WebBanHangOnline.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();   
         // GET: Product
-        public ActionResult Index()
+        public ActionResult Index(int? id, int? page)
         {
-            return View();
+            var pageSize = 8;
+            if (page == null)
+            {
+                page = 1;
+            }
+            IEnumerable<Product> items = db.Products.OrderByDescending(x => x.Id);
+            if (id != null)
+            {
+                items = items.Where(x => x.Id == id).ToList();
+            }
+            var pageIndex = page.HasValue ? Convert.ToInt32(page) : 1;
+            items = items.ToPagedList(pageIndex, pageSize);
+            ViewBag.PageSize = pageSize;
+            ViewBag.Page = page;
+            return View(items);
+        }
+        public ActionResult ProductCategory(int? page, int id)
+        {
+            var pageSize = 8;
+            if (page == null)
+            {
+                page = 1;
+            }
+            IEnumerable<Product> items = db.Products.OrderByDescending(x => x.Id);
+            if (id > 0)
+            {
+                items = items.Where(x => x.ProductCategoryId == id).ToList();
+            }
+            var cate = db.ProductCategories.Find(id);
+            if(cate != null)
+            {
+                ViewBag.CateName = cate.Title;
+            }
+            var pageIndex = page.HasValue ? Convert.ToInt32(page) : 1;
+            items = items.ToPagedList(pageIndex, pageSize);
+            ViewBag.CateId = id;
+            ViewBag.PageSize = pageSize;
+            ViewBag.Page = page;
+            return View(items);
         }
         public ActionResult Partial_ItemsByCateId()
         {
