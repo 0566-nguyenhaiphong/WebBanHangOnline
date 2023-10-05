d8216850d02e1d90c5e408bd52881dfa947774df
nguyễn phong <phongp1192002@phongp1192002.onmicrosoft.com>
2023-10-05 10:24:12 +0700

    Xóa, Cập nhật Giỏ hàng, Form thanh toán

--- WebBanHangOnline/Scripts/jsShopping.js
+++ WebBanHangOnline/Scripts/jsShopping.js
@@ -34,13 +34,29 @@
                     if (rs.Success) {
                         $('#checkout_items').html(rs.Count);
                         $('#trow_' + id).remove()
-                       
+                        LoadCart()
                     }
                 }
-            });
+            })
         }
         
+    })
+    $('body').on('click', '.btnUpdate', function (e) {
+        e.preventDefault();
+        var id = $(this).data('id')
+        var quantity = $('#Quantity_' + id).val()
+        UpdateCart(id, quantity)
+
     });
+    $('body').on('click', '.btnDeleteAll', function (e) {
+        e.preventDefault();
+        var conf = confirm('Bạn có muốn xóa giỏ hàng không ?')
+        if (conf) {         
+            DeleteAll()
+        }
+
+    })
+
 })
 function ShowCount() {
     $.ajax({
@@ -51,3 +67,37 @@ function ShowCount() {
         }
     });
 }
+function UpdateCart(id, quantity) {
+    $.ajax({
+        url: '/shoppingcart/UpdateCart',
+        type: 'POST',
+        data: { id: id, quantity: quantity },
+        success: function (rs) {
+            if (rs.Success) {
+                LoadCart()
+            }
+        }
+    });
+}
+
+function DeleteAll() {
+    $.ajax({
+        url: '/shoppingcart/DeleteAll',
+        type: 'POST',
+        success: function (rs) {
+            if (rs.Success) {
+                LoadCart()
+            }
+        }
+    });
+}
+function LoadCart() {
+    $.ajax({
+        url: '/shoppingcart/Partial_Item_Cart',
+        type: 'GET',
+        success: function (rs) {
+            $('#load_data').html(rs);
+        }
+    });
+}
+
