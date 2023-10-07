4cd9afd5f8194eb6344959a340be8f4a4201bf5b
nguyễn phong <phongp1192002@phongp1192002.onmicrosoft.com>
2023-10-07 22:01:20 +0700

    thanh toán

--- WebBanHangOnline/Scripts/jsShopping.js
+++ WebBanHangOnline/Scripts/jsShopping.js
@@ -16,7 +16,7 @@
             success: function (rs) {
                 if (rs.Success) {
                     $('#checkout_items').html(rs.Count);
-                    alert(rs.msg);
+                    showSuccessToast()
                 }
             }
         });
@@ -56,6 +56,28 @@
         }
 
     })
+    //$('body').on('click', '.submitBtn', function (e) {
+    //    $.ajax({
+    //        url: '/ShoppingCart/CheckOut',
+    //        type: 'POST',
+    //        data: $('#myForm').serialize(),
+    //        success: function (rs) {
+    //            if (rs.success) {
+
+    //                // Handle success (e.g., show a success message)
+    //                showSuccessToast();
+    //            }
+    //        },
+    //        error: function (rs) {
+    //            if (!rs.success) {
+
+    //                // Handle error (e.g., show an error message)
+    //                showErrorToast();
+    //            }
+
+    //        }
+    //    });
+    //});
 
 })
 function ShowCount() {
@@ -86,6 +108,7 @@ function DeleteAll() {
         type: 'POST',
         success: function (rs) {
             if (rs.Success) {
+                $('#checkout_items').html(rs.Count);
                 LoadCart()
             }
         }
@@ -99,5 +122,71 @@ function LoadCart() {
             $('#load_data').html(rs);
         }
     });
+}
+function showSuccessToast(rs) {
+    toast({
+        title: "Thành công!",
+        message: "Mua hàng thành công",
+        type: "success",
+        duration: 3000
+    });
+}
+
+
+
+function showErrorToast(rs) {
+    toast({
+        title: "Thất bại!",
+        message: "Có lỗi xảy ra, vui lòng liên hệ quản trị viên.",
+        type: "error",
+        duration: 3000
+    });
+
+}
+// Toast function
+function toast({ title = "", message = "", type = "info", duration = 3000 }) {
+    const main = document.getElementById("toast");
+    if (main) {
+        const toast = document.createElement("div");
+
+        // Auto remove toast
+        const autoRemoveId = setTimeout(function () {
+            main.removeChild(toast);
+        }, duration + 1000);
+
+        // Remove toast when clicked
+        toast.onclick = function (e) {
+            if (e.target.closest(".toast__close")) {
+                main.removeChild(toast);
+                clearTimeout(autoRemoveId);
+            }
+        };
+
+        const icons = {
+            success: "fa fa-check",
+            info: "fas fa-info-circle",
+            warning: "fas fa-exclamation-circle",
+            error: "fas fa-exclamation-circle"
+        };
+        const icon = icons[type];
+        const delay = (duration / 1000).toFixed(2);
+
+        toast.classList.add("toast", `toast--${type}`);
+        toast.style.animation = `slideInLeft ease .3s, fadeOut linear 1s ${delay}s forwards`;
+
+        toast.innerHTML = `
+                      <div class="toast__icon">
+                          <i class="${icon}"></i>
+                      </div>
+                      <div class="toast__body">
+                          <h3 class="toast__title">${title}</h3>
+                          <p class="toast__msg">${message}</p>
+                      </div>
+                      <div class="toast__close">
+                          <i class="fa fa-times"></i>
+                      </div>
+                  `;
+        main.appendChild(toast);
+    }
 }
 
