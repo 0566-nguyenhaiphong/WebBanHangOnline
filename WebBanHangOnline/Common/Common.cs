bf5061dc8e5ffe27f85d1464f82288fd4daf015a
nguyễn phong <phongp1192002@phongp1192002.onmicrosoft.com>
2023-10-08 21:31:31 +0700

    gửi mail khi đặt hàng thành công

---
+++ WebBanHangOnline/Common/Common.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Web;
+
+namespace WebBanHangOnline.Common
+{
+    public class Common
+    {
+        private static string password = ConfigurationManager.AppSettings["PasswordEmail"];
+        private static string Email = ConfigurationManager.AppSettings["Email"];
+        public static bool SendMail(string name, string subject, string content,
+            string toMail)
+        {
+            bool rs = false;
+            try
+            {
+                MailMessage message = new MailMessage();
+                var smtp = new SmtpClient();
+                {
+                    smtp.Host = "smtp.gmail.com"; //host name
+                    smtp.Port = 587; //port number
+                    smtp.EnableSsl = true; //whether your smtp server requires SSL
+                    smtp.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+
+                    smtp.UseDefaultCredentials = false;
+                    smtp.Credentials = new NetworkCredential()
+                    {
+                        UserName = Email,
+                        Password = password
+                    };
+                }
+                MailAddress fromAddress = new MailAddress(Email, name);
+                message.From = fromAddress;
+                message.To.Add(toMail);
+                message.Subject = subject;
+                message.IsBodyHtml = true;
+                message.Body = content;
+                smtp.Send(message);
+                rs = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                rs = false;
+            }
+            return rs;
+        }
+    }
+}
---
+++ WebBanHangOnline/Migrations/202310081026080_updateOrder.cs
@@ -0,0 +1,20 @@
+﻿namespace WebBanHangOnline.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updateOrder : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.tb_Order", "Email", c => c.String(nullable: false));
+            AlterColumn("dbo.tb_Order", "Address", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.tb_Order", "Address", c => c.String(nullable: false));
+            DropColumn("dbo.tb_Order", "Email");
+        }
+    }
+}
