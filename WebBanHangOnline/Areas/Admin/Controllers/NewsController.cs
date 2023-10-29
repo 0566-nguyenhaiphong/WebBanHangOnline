ef4a0861bbfcee9b74f668549d3a60dd2a13f09c
nguyễn phong <phongp1192002@phongp1192002.onmicrosoft.com>
2023-10-29 17:20:56 +0700

    tạo trang tin tức ở bên khách hàng

--- WebBanHangOnline/Areas/Admin/Controllers/NewsController.cs
+++ WebBanHangOnline/Areas/Admin/Controllers/NewsController.cs
@@ -37,19 +37,21 @@ namespace WebBanHangOnline.Areas.Admin.Controllers
         {
             return View();
         }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Add(News model)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
-                model.CreatedDate = DateTime.Now;   
-                model.ModifiedDate = DateTime.Now;
+                model.CreatedDate = DateTime.Now;
                 model.CategoryId = 3;
+                model.ModifiedDate = DateTime.Now;
                 model.Alias = WebBanHangOnline.Models.Common.Filter.FilterChar(model.Title);
-                db.News.Add(model);
+                db.News.Attach(model);
+                db.Entry(model).State = System.Data.Entity.EntityState.Modified;
                 db.SaveChanges();
-                return RedirectToAction("Index");   
+                return RedirectToAction("Index");
             }
             return View(model);
         }
@@ -58,6 +60,7 @@ namespace WebBanHangOnline.Areas.Admin.Controllers
             var item = db.News.Find(id);
             return View(item);
         }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit(News model)
@@ -65,7 +68,6 @@ namespace WebBanHangOnline.Areas.Admin.Controllers
             if (ModelState.IsValid)
             {
                 model.ModifiedDate = DateTime.Now;
-                model.CategoryId = 3;
                 model.Alias = WebBanHangOnline.Models.Common.Filter.FilterChar(model.Title);
                 db.News.Attach(model);
                 db.Entry(model).State = System.Data.Entity.EntityState.Modified;
